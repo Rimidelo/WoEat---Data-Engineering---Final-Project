99decefbd8cc9bbc34c33c48dc3e5e58af17b540
rootsUnicourse <rootsunicourse@gmail.com>
2025-06-07 14:49:56 +0300

    working create_table.py

--- processing/create_table_test.py
+++ processing/create_table_test.py
@@ -8,13 +8,8 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 spark = (
     SparkSession.builder
         .appName("WoEat – bronze.test_drivers demo")
-        .config("spark.sql.catalog.demo",              "org.apache.iceberg.spark.SparkCatalog")
-        .config("spark.sql.catalog.demo.catalog-impl", "org.apache.iceberg.rest.RESTCatalog")
-        .config("spark.sql.catalog.demo.uri",          "http://rest:8181")
-        .config("spark.sql.catalog.demo.io-impl",      "org.apache.iceberg.aws.s3.S3FileIO")
-        .config("spark.sql.catalog.demo.s3.endpoint",  "http://minio:9000")
         .config("spark.sql.catalog.demo.s3.path-style-access", "true")
-        .config("spark.sql.catalog.demo.s3.access-key-id",     "admin")
+        .config("spark.sql.catalog.demo.s3.access-key-id", "admin")
         .config("spark.sql.catalog.demo.s3.secret-access-key", "password")
         .getOrCreate()
 )
